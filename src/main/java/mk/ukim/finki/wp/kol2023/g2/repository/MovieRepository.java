d48d0ce2d80e587f2f3f9c8e9adb98c6558efa6a
Stefan Denkovikj <stefandenkovik123@gmail.com>
2024-02-26 00:16:29 +0100

    First Commit

---
+++ src/main/java/mk/ukim/finki/wp/kol2023/g2/repository/MovieRepository.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.wp.kol2023.g2.repository;
+
+import mk.ukim.finki.wp.kol2023.g2.model.Genre;
+import mk.ukim.finki.wp.kol2023.g2.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+    List<Movie> findByRatingLessThan(Double rating);
+    List<Movie> findByGenre(Genre genre);
+    List<Movie> findByRatingLessThanAndGenre(Double rating,Genre genre);
+}
